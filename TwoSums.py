2000282473878e1497e83556ca098d87c2d5b24d
Chelsea Lin <chelsea.lin.08@gmail.com>
2025-07-24 16:37:17 -0400

    new file

---
+++ TwoSums.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = {}
+        for i, num in enumerate(nums):
+            compatible = target - num
+            if compatible in map:
+                return [map[compatible], i]
+            map[num] = i
+        return []
+        
